3e6755635d4abc39105844de7a53b2e64638e4f4
TanerMnkl <124084945+TanerMnkl@users.noreply.github.com>
2023-02-07 13:15:08 +0300

    ilkverilenödev

---
+++ ödev1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ödev1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //klavyeden girilen iki adet öğrenci notunun ortalamasını bulup, ortalama 50 den büyük ya da eşitse ekrana geçti degilse kaldı yazdıran programı hazırlayın. öğrenci ortalaması 100 üzerinden girilecektir.
+            //bir üni ögrencisine ait vize ve final notları 100 üzerinden klavyeden girilecektir.bnaşarı puanaı vize %40 final %60 ve geçme notu 60 -60 ve üzeri başarılı altı ise başarısız.
+            //klavyeden bir sayı girilmesini ve ardından istege göre bu sayının karesini ya da küpünü hesaplayan programı hazrılayınız.
+
+            #region 1. soru
+
+
+            //Console.WriteLine("Birinci notu giriniz: ");
+            //int sayı1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("İkinci notu giriniz: ");
+            //int sayı2 = Convert.ToInt32(Console.ReadLine());
+            //int sonuc = Convert.ToInt32((sayı1 + sayı2) / 2);
+            //if (sonuc > 100)
+            //{
+            //    Console.WriteLine("hatalı bir not girdiniz ");
+
+            //}else if(sonuc>=50)
+            //{
+            //    Console.WriteLine("geçti");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Kaldı");
+            //}
+
+
+
+
+
+
+
+
+            #endregion
+
+            #region 2.soru
+
+
+            //Console.WriteLine("Vize notunuzu giriniz");
+            //int vize = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("buraya final notunuzu giriniz");
+            //int final = Convert.ToInt32(Console.ReadLine());
+            //int sonuc = ((vize * 40 + final * 60) / 100);
+
+
+            //if (sonuc > 100)
+            //{
+            //    Console.WriteLine("hatalı bir sonuç girdiniz");
+
+            //}else if (sonuc>=60)
+            //{
+            //    Console.WriteLine("başarılı");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("BAşarısız");
+            //}
+
+
+
+            #endregion
+
+
+
+            #region 3.soru
+            //Console.WriteLine("lütfen bir sayı giriniz");
+            //int sayı = int.Parse(Console.ReadLine());
+            //Console.WriteLine("bu sayının karesini mi hesaplamak istersiniz küpünü mü ?(karesi ya da küpü yazınız):");
+            //string seçenek = Console.ReadLine();
+
+            //if (seçenek =="karesi")
+            //{
+            //    int karesi = sayı * sayı;
+            //    Console.WriteLine("karesi alınan sayı: " + karesi);
+            //}else if(seçenek=="küpü") 
+            //{
+            //    int küpü = sayı * sayı * sayı;
+            //    Console.WriteLine("küpü alınan sayı: " + küpü );
+            //}
+            //else
+            //{
+            //    Console.WriteLine("lütfen karesi ya da küpü yazınız");
+            //}
+
+
+
+            #endregion
+
+
+
+        }
+
+    }
+}
