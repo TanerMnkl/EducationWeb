5ceaf9f5ab823802c26bfad6dd6f5f6b97411732
TanerMnkl <124084945+TanerMnkl@users.noreply.github.com>
2023-06-08 00:58:01 +0300

    07-06-2023-Hata Çözülemedi

---
+++ 07-06-2023/07-06-2023/BooksApp/BooksApp.MVC/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BooksApp.MVC.Models
+{
+    public class LoginViewModel
+    {
+        [DisplayName("Kullanıcı Adı")]
+        [Required(ErrorMessage ="Kullanıcı adı boş bırakılmamalıdır.")]
+        public string UserName { get; set; }
+
+        [DisplayName("Şifre")]
+        [Required(ErrorMessage ="Şifre Boş bırakılmamalıdır.")]
+        
+        public string Password { get; set; }
+    }
+}
